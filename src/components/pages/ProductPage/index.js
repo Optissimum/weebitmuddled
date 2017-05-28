d64c6102509633b316d5e039d106847bb28cb132
Sebastian MacDonald <optissimum@gmail.com>
2017-05-28 18:16:32 -0400

    Remove excessive product generation

--- src/components/pages/ProductPage/index.js
+++ src/components/pages/ProductPage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { PageTemplate, Header, Footer, ProductList } from 'components'
 // import { ProductList } from 'containers'
 
-const products = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]
+const products = [1, 2, 3, 4, 5]
 
 const ProductPage = () => {
   return (
